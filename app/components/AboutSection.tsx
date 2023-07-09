3c35f882b3bea5c5829016fe84f838ec20ccee7a
Raji Sarafadeen Sanjo <omosanjos77@gmail.com>
2023-07-09 13:30:20 +0100

    first committ of this portfolio project

---
+++ app/components/AboutSection.tsx
@@ -0,0 +1,84 @@
+import React from "react"
+import Image from "next/image"
+
+const skills = [
+  { skill: "HTML" },
+  { skill: "CSS" },
+  { skill: "JavaScript" },
+  { skill: "React" },
+  { skill: "Next.js" },
+  { skill: "Tailwind CSS" },
+  { skill: "Styled-component" },
+  { skill: "chakra-ui" },
+  { skill: "Git" },
+  { skill: "GitHub" },
+  { skill: "redux" },
+]
+
+const AboutSection = () => {
+  return (
+    <section id="about">
+      <div className="my-12 pb-12 md:pt-16 md:pb-48">
+        <h1 className="text-center font-bold text-4xl">
+          About Me
+          <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+        </h1>
+
+        <div className="flex flex-col space-y-10 items-stretch justify-center align-top md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left">
+          <div className="md:w-1/2 ">
+            <h1 className="text-center text-2xl font-bold mb-6 md:text-left">
+              Get to know me!
+            </h1>
+            <p>
+              Hi, my name is Raji Sarafadeen Sanjo and I am a{" "}
+              <span className="font-bold">{"highly passionate"}</span>,
+              <span className="font-bold">{" self-motivated"}</span>, and
+              <span className="font-bold">{" driven"}</span> software engineer
+              
+            </p>
+            <br />
+            <p>
+             As a proactive learner, I expanded my skills in web development through esteemed platforms like Coursera in HTML, CSS, and JavaScript (2021 certificate) and SoloLearn (2021). Additionally, I gained practical experience as an intern at Kodekamp (2022), solidifying my proficiency in ReactJS and furthering my passion for creating remarkable digital experiences.
+            </p>
+            <br />
+            <p>
+           Avidly Pursuing My Passions: Reading, Sports, Traveling. Constantly Seeking Fresh Experiences, I Engage in Diverse Hobbies, Always Learning.
+            </p>
+            <br />
+            <p>
+              I believe that you should{" "}
+              <span className="font-bold text-teal-500">
+                never stop growing
+              </span>{" "}
+              and that&#39;s what I have a strong passion for technology and constantly push its limits. I'm excited to see where my career takes me and always open to new opportunities that come my way. 🙂
+            </p>
+          </div>
+          <div className="text-center md:w-1/2 md:text-left">
+            <h1 className="text-2xl font-bold mb-6">My Skills</h1>
+            <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
+              {skills.map((item, idx) => {
+                return (
+                  <p
+                    key={idx}
+                    className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
+                  >
+                    {item.skill}
+                  </p>
+                )
+              })}
+            </div>
+            <Image
+              src="/about.png"
+              alt=""
+              width={325}
+              height={325}
+              className="hidden md:block md:relative md:bottom-4 md:left-28 md:z-0"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default AboutSection
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,96 @@
+import {Link} from "react-scroll/modules";
+import React, { useState } from "react";
+ import {useTheme} from "next-themes";
+import {RiMoonFill,RiSunLine} from "react-icons/ri";
+import { IoMdMenu, IoMdClose } from "react-icons/io"
+
+interface NavItem{
+    label:string,
+    page:string,
+}
+const NAV_ITEMS: Array<NavItem> =[
+    {
+        label:"Home",
+        page:"home"
+    },
+    {
+        label:"About",
+        page:"about"
+    },
+    {
+        label:"Projects",
+        page:"projects"
+    },
+]
+const Navbar = () => {
+    const { systemTheme, theme, setTheme } = useTheme()
+    const currentTheme = theme === "system" ? systemTheme : theme
+    const [navbar, setNavbar] = useState(false)
+  return (
+  <header className={`w-full mx-auto py-2 px-4 sm:px-20 fixed top-0 z-50 shadow ${currentTheme === 'dark' && 'bg-stone-900 z-50' } ${currentTheme === 'light' && 'bg-white z-50' }  z-50 dark:bg-stone-950 dark:border-b dark:border-stone-600`}>
+    <nav className="justify-between md:items-center md:flex">
+      <div>
+          <div className="flex items-center justify-between py-3 md:py-5 md:block">
+            <div className="md:py-5 md:block">
+              <Link to='home'>
+                <h2 className="cursor-pointer text-2xl font-bold">Raji | S | S</h2>
+              </Link>
+            </div>
+            <div className="md:hidden">
+                <button onClick={()=>setNavbar(pre=>!pre)}>{navbar ? <IoMdClose size={25}/> : <IoMdMenu size={25}/> }</button>
+            </div>
+        </div>
+      </div>
+
+      <div>
+
+       <div
+            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+              navbar ? "block" : "hidden"
+            }`}
+          >
+
+        <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+            {NAV_ITEMS.map((item, index)=>{
+                return(
+                <li 
+                className="cursor-pointer"                     
+                key={index}
+                onClick={() => setNavbar(!navbar)}>
+                    
+              <Link
+                to={item.page}
+                className="block lg:inline-block   hover:text-neutral-500 dark:text-neutral-100"
+                activeClass="active"
+                spy={true}
+                smooth={true}
+                offset={-100}
+                duration={500}>
+                {item.label}
+              </Link>
+                </li>
+                )
+            })}
+            {currentTheme === 'dark' ? 
+                <button onClick={()=>{setTheme("light")}}
+                className="bg-slate-100 p-2 rounded-xl"
+                >
+                    <RiSunLine size={25} color="black"/>
+                </button>
+            :
+            <button 
+            onClick={()=>{setTheme("dark")}}
+            className="bg-slate-100 p-2 rounded-xl"
+            >
+                <RiMoonFill size={25}/>
+            </button>
+            }
+        </ul>
+      </div>
+      </div>
+    </nav>
+  </header>
+  )
+};
+
+export default Navbar;
